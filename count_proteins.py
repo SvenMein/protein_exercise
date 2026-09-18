1962917942845d9d2875f60e111676b4ffba0a56
saif21 <md_saiful.islam@alumni.uni-heidelberg.de>
2026-09-18 14:32:09 +0200

    Add protein counting function

--- count_proteins.py
+++ count_proteins.py
@@ -1,14 +1,15 @@
+from Bio import SeqIO
+
+
 def count_proteins(fasta_file):
     count = 0
 
-    with open(fasta_file, "r") as file:
-        for line in file:
-            if line.startswith(">"):
-                count += 1
+    for protein in SeqIO.parse(fasta_file, "fasta"):
+        count += 1
 
     return count
 
 
 protein_count = count_proteins("gfp.fasta")
 
-print("Number of proteins:", protein_count)
+print(f"Number of proteins: {protein_count}")
