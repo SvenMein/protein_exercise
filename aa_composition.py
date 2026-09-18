f4dc7711f5b748856f6bba1a6faf74424e72add6
cleerix <clemensrichter@me.com>
2026-09-18 13:34:57 +0200

    aa_composition comments added

---
+++ aa_composition.py
@@ -0,0 +1,8 @@
+#### amino acid composition program ####
+
+# read the protein sequences from the fasta file
+# count the different amino acids from the sequences
+# print out the name of the protein
+# print out the amino acid composition
+
+# hallo
